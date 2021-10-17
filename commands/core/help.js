3504ab16b2f238a7a1e06b11967c8ad89abcab8f
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-10-17 17:05:02 +0530

    Updated files

--- commands/core/help.js
+++ commands/core/help.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: '$%^$@',
+    name: '$%>$@',
     aliases: ['nothingNew'],
     showHelp: false,
     utilisation: '{prefix}music',
