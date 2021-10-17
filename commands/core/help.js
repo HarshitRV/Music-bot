40dd442ee2d0f4914bcf3b92d700b483df66168a
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-10-17 15:44:11 +0530

    initial commit

---
+++ commands/core/help.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    aliases: ['m'],
+    showHelp: false,
+    utilisation: '{prefix}music',
+
+    execute(client, message, args) {
+        const embed = new MessageEmbed();
+
+        embed.setColor('0x0949EE');
+        embed.setAuthor(client.user.username, client.user.displayAvatarURL({ size: 1024, dynamic: true }));
+
+        const commands = client.commands.filter(x => x.showHelp !== false);
+
+        embed.setDescription("**Commands**");
+        embed.addField(`Enabled - ${commands.size}`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
+
+        embed.setTimestamp();
+        embed.setFooter('Requested by '+message.author.username , message.author.avatarURL({ dynamic: true }));
+
+        message.channel.send({ embeds: [embed] });
+    },
+};
---
+++ commands/music/save.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'save',
+    aliases: ['sv'],
+    utilisation: '{prefix}save',
+    voiceChannel: true,
+
+    async execute(client, message) {
+        const queue = player.getQueue(message.guild.id);
+
+        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+
+        message.author.send(`You saved the track ${queue.current.title} | ${queue.current.author} from the server ${message.guild.name} ✅`).then(() => {
+            message.channel.send(`Sent the title of the music by private messages ✅\nMake sure that your DMs are open 😊`);
+        }).catch(error => {
+            message.channel.send(`Unable to send you a private message ${message.author}... try again ? ❌`);
+        });
+    },
+};
