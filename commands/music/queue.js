5deac34a0ed74509c577d7f60cb63f3b09792386
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-10-17 20:00:34 +0530

    Updated Queue file

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -16,7 +16,7 @@ module.exports = {
         const embed = new MessageEmbed();
         const methods = ['', '🔁', '🔂'];
 
-        embed.setColor('RED');
+        embed.setColor('0x0949EE');
         embed.setThumbnail(message.guild.iconURL({ size: 2048, dynamic: true }));
         embed.setAuthor(`Server queue - ${message.guild.name} ${methods[queue.repeatMode]}`, client.user.displayAvatarURL({ size: 1024, dynamic: true }));
 
@@ -28,7 +28,7 @@ module.exports = {
         embed.setDescription(`Current ${queue.current.title}\n\n${tracks.slice(0, 5).join('\n')}\n\n${nextSongs}`);
 
         embed.setTimestamp();
-        embed.setFooter('Music comes first - Made with heart by Zerio ❤️', message.author.avatarURL({ dynamic: true }));
+        embed.setFooter(`Requested by ${message.author.username}`, message.author.avatarURL({ dynamic: true }));
 
         message.channel.send({ embeds: [embed] });
     },
